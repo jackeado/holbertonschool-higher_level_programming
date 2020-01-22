e4a46bbf55cefbef36658d7d6f418547c4e33f8b
jackeado <pabsanchez@gmail.com>
2020-01-22 00:14:45 -0500

    fix

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -6,5 +6,7 @@ Cotains read file function
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, encoding='utf-8', mode="w")
-        git
+    """function that writes an Object to a text file,\
+     using a JSON representation"""
+    with open(filename, encoding='utf-8', mode="w") as file:
+        json.dump(my_obj, file)
