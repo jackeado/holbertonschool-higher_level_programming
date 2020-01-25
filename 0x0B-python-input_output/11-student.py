9eb192f34c314035ad359ba472fb0a38f3894b8f
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-01-25 11:04:08 -0500

    10 to 13

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,18 @@
+#!/usr/binpython3
+
+"""
+Contains a class Student
+"""
+
+
+class Student:
+    """Representation of Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of a Student instance"""
+        return self.__dict__
