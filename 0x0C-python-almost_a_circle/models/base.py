c2db98f26b2063ebff6ad07eaa63817787ab924d
jackeado <pabsanchez@gmail.com>
2020-01-27 18:09:51 -0500

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,13 +10,14 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-       if id is None:
-           Base.__nb_objects += 1
-           self.id = Base.__nb_objects
-       else:
-           self.id = id
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+
     @classmethod
-    def to_json_string(list_dictionaries):
+    def to_json_string(cls, list_dictionaries):
         if list_dictionaries is None:
             return "[]"
         else:
@@ -26,20 +27,18 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         filename = "{}.json".format(cls.__name__)
-        dic = []
+        lista_d = []
         if list_objs is not None:
             for inst in list_objs:
-                dic.append(cls.to_dictionary(dic))
-        with open(filename, encoding='utf-8', mode='w') as json_file:
-            json_file.write(cls.to_json_string(dic))
+                lista_d.append(inst.to_dictionary())
+        with open(filename, encoding='utf-8', mode='w') as f:
+            print(cls.to_json_string(lista_d))
 
-    # def save_to_file(cls, list_objs):
-    #     """writes the JSON string representation of list_objs to a file"""
-    #     filename = "{}.json".format(cls.__name__)
-    #     dic = []
-    #     if list_objs is not None:
-    #         for i in list_objs:
-    #             dic.append(cls.to_dictionary(i))
-    #     with open(filename, 'w', encoding="utf8") as n_file:
-    #         n_file.write(cls.to_json_string(dic))
-
+    @classmethod
+    def from_json_string(cls, json_string):
+        if json_string is None or json_string == 0:
+            lista_ = []
+            return lista_
+        else:
+            lista_ = json.loads(json_string)
+            return lista_
