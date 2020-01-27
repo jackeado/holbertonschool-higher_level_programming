c360c32ba0fd2d990e0a8bf42640ffc95bb00335
jackeado <pabsanchez@gmail.com>
2020-01-27 18:47:31 -0500

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     """Initialize the base Square"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
