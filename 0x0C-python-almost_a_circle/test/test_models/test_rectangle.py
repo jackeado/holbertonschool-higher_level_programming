afbab3ec30005f56f86beac395018f2a6d8b76e5
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-01-28 00:15:30 -0500

    fix pep8

--- 0x0C-python-almost_a_circle/test/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/test/test_models/test_rectangle.py
@@ -10,11 +10,11 @@ class Testrectangle(unittest.TestCase):
     """ """
 
     def test_pep8_conformance_rectangle(self):
-    	"""Test that we conform to PEP8."""
-    	pep8style = pep8.StyleGuide(quiet=True)
-    	result = pep8style.check_files(['models/rectangle.py'])
-    	self.assertEqual(result.total_errors, 0,
-                        "Found code style errors (and warnings).")
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_subclass(self):
         self.assertTrue(issubclass(Rectangle, Base))
