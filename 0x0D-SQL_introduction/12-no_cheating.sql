fae8f7884b493eb2b75d77eaf4d3050692aea409
jackeado <pabsanchez@gmail.com>
2020-03-03 15:37:47 -0500

    fix

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- updates the score of Bob to 10 in the table second_table
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+UPDATE second_table SET score=10 WHERE name="Bob";
