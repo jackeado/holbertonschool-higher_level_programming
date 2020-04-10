c7256117ccf4313e715b815750f26fc3c13751ba
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-04-10 18:22:53 -0500

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
