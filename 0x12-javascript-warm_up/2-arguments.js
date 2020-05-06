82210aaa0e33f7119208c1d901486c4358bb4e84
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-06 09:56:40 -0500

    fix executable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-let arg = process.argv.length;
-if (arg === 2) {
-    console.log("No argument");
+const arg = process.argv.length;
+if (arg < 3) {
+  console.log('No argument');
 } else if (arg === 3) {
-    console.log("Argument found");
+  console.log('Argument found');
 } else {
-    console.log("Arguments found");
+  console.log('Arguments found');
 }
