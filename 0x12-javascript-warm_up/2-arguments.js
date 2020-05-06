402a3c6b4d6b58ed3d4363c75d7929dcb40b9378
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-06 00:41:41 -0500

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-let argu = process.argv.length;
-if (argu === 2) {
+let arg = process.argv.length;
+if (arg === 2) {
     console.log("No argument");
-} else if (argu === 3) {
+} else if (arg === 3) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
