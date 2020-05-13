3cd5596845036a4504287117af31f818779072df
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-12 22:15:32 -0500

    first file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+ // Read a file
+
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data);
+    }
+});
