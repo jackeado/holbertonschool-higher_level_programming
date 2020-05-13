a137f319a2e09e5337bcc46ee811ce8c1e363697
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-12 23:29:41 -0500

    fix url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
-// Star Wars API
+ // Star Wars API
 const request = require('request');
-const requestURL = 'https://swapi-api.hbtn.io/';
+const requestURL = 'https://swapi-api.hbtn.io/api/films/';
 const episode = process.argv[2];
-request(requestURL + episode, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
-    const epi = JSON.parse(body);
-    console.log(epi.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+request(requestURL + episode, function(error, response, body) {
+    if (error) {
+        console.log(error);
+    } else if (response.statusCode === 200) {
+        const epi = JSON.parse(body);
+        console.log(epi.title);
+    } else {
+        console.log('An error occured. Status code: ' + response.statusCode);
+    }
 });
