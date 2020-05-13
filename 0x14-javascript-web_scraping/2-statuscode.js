054d18aaeacdbea2f57ec603d1da51abb463fd36
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-12 22:58:33 -0500

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
- // status code
+// status code
 const request = require('request');
 const info = process.argv[2];
-request(info, function(err, response) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('code: ' + response.statusCode);
-    }
+request(info, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
