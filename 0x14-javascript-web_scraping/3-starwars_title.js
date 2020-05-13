d09714619dede40e99f42a4a9a0a4bd52558efc9
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-12 23:25:55 -0500

    star wars API

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Star Wars API
+const request = require('request');
+const requestURL = 'https://swapi-api.hbtn.io/';
+const episode = process.argv[2];
+request(requestURL + episode, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const epi = JSON.parse(body);
+    console.log(epi.title);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
