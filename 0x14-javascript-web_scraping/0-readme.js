d11d69dc9b0c3fb253e80e5dfbf8fd4b530c4c90
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2020-05-12 22:17:14 -0500

    fix semistandar

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
- // Read a file
+// Read a file
 
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf-8', function(err, data) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(data);
-    }
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
